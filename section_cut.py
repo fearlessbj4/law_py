8afdf71e34855b3f68c882b1c31a04da269d0bde
fearlessbj4 <59113578+fearlessbj4@users.noreply.github.com>
2021-08-20 02:54:09 +0800

    Add files via upload

---
+++ section_cut.py
@@ -0,0 +1,34 @@
+def cut(lst,mode,start_point,mid_point,end_point,start_list,mid_list,end_list):
+	shallow_stp=start_point
+	shallow_mdp=mid_point
+	shallow_edp=end_point
+	for sti in range(len(start_list)):
+		if(lst.find(start_list[sti])!=-1):
+			shallow_stp=start_list[sti]
+			break
+		else:
+			continue
+	for mdi in range(len(mid_list)):
+		if(lst.find(mid_list[mdi])!=-1):
+			shallow_mdp=mid_list[mdi]
+			break
+		else:
+			continue
+	for edi in range(len(end_list)):
+		if(lst.find(end_list[edi])!=-1):
+			shallow_edp=end_list[edi]
+			break
+		else:
+			continue
+
+	r_lst=lst[lst.find(shallow_stp)+len(shallow_stp):lst.find(shallow_edp)]
+	
+
+	if(mode=="short"):
+		
+		return r_lst[:r_lst.find(shallow_mdp)]
+	elif(mode=="mid"):
+		return r_lst[r_lst.find(shallow_mdp):]
+	else:
+		return r_lst
+
